b6234680e0a95e4dca9888359c7d6f7f3397d687
Meik Tranel <tranelmeik@gmail.com>
2018-01-28 18:22:13 +0100

    Added method for first time startup
    Added admin etc. so that a database may be initialized from the application
    added Rental business object and further implemented the mapping config

---
+++ src/Videothek.Core/Rental.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Videothek.Core
+{
+    public class Rental
+    {
+        public int ID { get; set; }
+        public Video Video { get; set; }
+        public DateTime Date { get; set; }
+        public User User { get; set; }
+    }
+}
